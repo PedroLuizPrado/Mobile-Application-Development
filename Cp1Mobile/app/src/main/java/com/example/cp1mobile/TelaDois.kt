b1770c923449849c45931f73d35c1230b10a270f
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-06 12:05:51 -0300

    segunda tela

--- Cp1Mobile/app/src/main/java/com/example/cp1mobile/TelaDois.kt
+++ Cp1Mobile/app/src/main/java/com/example/cp1mobile/TelaDois.kt
@@ -1,6 +1,8 @@
 package com.example.cp1mobile
 
+import android.media.Image
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,9 @@ class TelaDois : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_dois)
+
+        var imageShadow: ImageView = findViewById(R.id.imageShadow)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
